5666051292504e5507b3f3defef13b1721d315e0
sunan-rabbi <sunanrabbi1918@outlook.com>
2024-10-08 22:23:32 +0600

    debug

--- src/app/modules/schedule/schedule.services.ts
+++ src/app/modules/schedule/schedule.services.ts
@@ -12,6 +12,7 @@ const convertDateTime = async (date: Date) => {
 }
 
 const insertIntoDB = async (payload: ISchedule): Promise<Schedule[]> => {
+
   const { startDate, endDate, startTime, endTime } = payload;
 
   const interverlTime = 30;
@@ -79,66 +80,6 @@ const insertIntoDB = async (payload: ISchedule): Promise<Schedule[]> => {
 
   return schedules;
 };
-
-// const getAllFromDB = async (
-//   filters: IScheduleFilterRequest,
-//   options: IPaginationOptions,
-// ): Promise<IGenericResponse<Schedule[]>> => {
-//   const { limit, page, skip } = paginationHelpers.calculatePagination(options);
-//   const { ...filterData } = filters;
-
-//   const andConditions = [];
-
-//   // if (searchTerm) {
-//   //   andConditions.push({
-//   //     OR: patientSearchableFields.map(field => ({
-//   //       [field]: {
-//   //         contains: searchTerm,
-//   //         mode: 'insensitive',
-//   //       },
-//   //     })),
-//   //   });
-//   // }
-
-//   if (Object.keys(filterData).length > 0) {
-//     andConditions.push({
-//       AND: Object.keys(filterData).map(key => {
-//         return {
-//           [key]: {
-//             equals: (filterData as any)[key],
-//           },
-//         };
-//       }),
-//     });
-//   }
-
-//   const whereConditions: Prisma.ScheduleWhereInput =
-//     andConditions.length > 0 ? { AND: andConditions } : {};
-
-//   const result = await prisma.schedule.findMany({
-//     where: whereConditions,
-//     skip,
-//     take: limit,
-//     orderBy:
-//       options.sortBy && options.sortOrder
-//         ? { [options.sortBy]: options.sortOrder }
-//         : {
-//           createdAt: 'desc',
-//         },
-//   });
-//   const total = await prisma.schedule.count({
-//     where: whereConditions,
-//   });
-
-//   return {
-//     meta: {
-//       total,
-//       page,
-//       limit,
-//     },
-//     data: result,
-//   };
-// };
 
 const getAllFromDB = async (
   filters: IScheduleFilterRequest,
